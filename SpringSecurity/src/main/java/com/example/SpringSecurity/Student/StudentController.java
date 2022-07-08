b6b0724ea98263e4456a4aa06c949249ba2850fa
789273 <rohitgunjal547@gmail.com>
2022-07-08 15:47:32 +0530

    commited

---
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/Student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.SpringSecurity.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController{
+    private static final List<Student>students= Arrays.asList(
+            new Student(1,"james"),
+            new Student(2,"maria"),
+            new Student(3,"Anna"),
+            new Student(4,"manson")
+    );
+    @GetMapping(path = "{studentId}")
+    public Student getStudentId(@PathVariable("studentId") Integer studentId){
+     return students.stream()
+             .filter(student -> studentId.equals(student.getStudentId()))
+             .findFirst()
+             .orElseThrow(()->new IllegalStateException("student "+studentId+" does not exist"));
+    }
+
+}
